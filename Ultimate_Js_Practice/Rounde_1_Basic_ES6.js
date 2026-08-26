0cc1ebca2ac468895a274f008b918b1931e5a969
SyedSami07 <syedsamigithub.com>
2026-08-26 22:04:55 +0600

    Exploring JS es6 from my friend

---
+++ Ultimate_Js_Practice/Rounde_1_Basic_ES6.js
@@ -0,0 +1,10 @@
+// Function: greet("nusu")
+// Output: Hello, nusu!
+// Use: arrow function + template literal
+
+
+const greet = (name) => {
+    return `hello, ${name}!`
+}
+
+console.log(greet("nusu"))
