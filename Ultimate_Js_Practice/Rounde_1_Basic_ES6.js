550afb5b21a6adaf9ee88b9155037091a460ace1
SyedSami07 <syedsamigithub.com>
2026-08-26 22:08:20 +0600

    Exploring JS es6 from my friend

--- Ultimate_Js_Practice/Rounde_1_Basic_ES6.js
+++ Ultimate_Js_Practice/Rounde_1_Basic_ES6.js
@@ -1,3 +1,4 @@
+// 1. Greeting
 // Function: greet("nusu")
 // Output: Hello, nusu!
 // Use: arrow function + template literal
@@ -7,4 +8,16 @@ const greet = (name) => {
     return `hello, ${name}!`
 }
 
-console.log(greet("nusu"))
+// console.log(greet("nusu"))
+
+
+
+
+// 2. Default Parameter
+// function: makeCoffee()
+// Output: Making coffee with 1 spoon of sugar.
+
+const makeCoffee = (sugar = 1) => {
+    return `Making coffee with ${sugar} spoon of sugar`
+}
+console.log(makeCoffee(3))
